2e3fae89c29ba8e1112decf280900c6aac31d072
Dmitry Zhuk <zhuchara@gmail.com>
2014-12-02 18:40:57 +0200

    Renamed rollstats.rb to rolls_stats.rb   Added a possibility of selectiong dice types in rolls_stats.

--- 03/dungeon/dice.rb
+++ 03/dungeon/dice.rb
@@ -1,19 +1,11 @@
 class RollDice
 
-  def roll
+  def d20
     rand(20)+1
   end
 
-end
+  def d6
+    rand(6)+1
+  end
 
-# dice = RollDice.new
-#
-# rolls = Array.new
-#
-# 10000.times { |x| rolls.insert(x, dice.roll)}
-#
-# stats = Hash.new
-#
-# rolls.each { |roll| stats[roll] += 1 }
-#
-# stats.each {|k, v| puts "#{k} rolled \t #{v} times"}
+end
---
+++ 03/dungeon/rolls_stats.rb
@@ -0,0 +1,37 @@
+# require '~/MoC-RoR/moc-ruby-2014-homework/03/dungeon/dice.rb'
+require './dice'
+
+
+
+dice = RollDice.new
+
+rolls = []
+
+freq = Hash.new(0)
+
+rolldice_methods = RollDice.instance_methods(false)
+
+puts 'Dice Types Present:'
+
+puts   '╔════╦═══════════╗'
+puts   "║ No ║ Dice Type ║"
+
+rolldice_methods.each_with_index do |item, index|
+  puts '╠════╬═══════════╣'
+  puts "║ #{index+1}  ║    #{item.to_s}\t ║"
+end
+
+puts   '╚════╩═══════════╝'
+
+d = gets.chomp.to_i - 1
+
+print 'Enter number of rolls: '
+n = gets.chomp.strip.to_i
+
+puts "You've rolled dice for #{n} times. And the result is: \n"
+
+n.times {|r| rolls.insert(r, dice.send(rolldice_methods[d]))}
+
+rolls.each {|r| freq[r] += 1}
+
+freq.sort.each {|k, v| puts "#{k}: \t #{v}"}
