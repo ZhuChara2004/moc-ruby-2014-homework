3240ed816f30d0adbad49e6fcf7a97c6a3901179
Dmitry Zhuk <zhuchara@gmail.com>
2014-12-02 20:05:35 +0200

    Improved appearance of responces in rolls_stats.rb

--- 03/dungeon/rolls_stats.rb
+++ 03/dungeon/rolls_stats.rb
@@ -14,7 +14,7 @@ rolldice_methods = RollDice.instance_methods(false)
 puts 'Dice Types Present:'
 
 puts   '╔════╦═══════════╗'
-puts   "║ No ║ Dice Type ║"
+puts   '║ No ║ Dice Type ║'
 
 rolldice_methods.each_with_index do |item, index|
   puts '╠════╬═══════════╣'
@@ -23,15 +23,27 @@ end
 
 puts   '╚════╩═══════════╝'
 
+print 'Select number of dice type: '
 d = gets.chomp.to_i - 1
+if d >= 0 && d <= rolldice_methods.index(rolldice_methods.last)
+  print 'Enter number of rolls: '
 
-print 'Enter number of rolls: '
-n = gets.chomp.strip.to_i
+  n = gets.chomp.strip.to_i
 
-puts "You've rolled dice for #{n} times. And the result is: \n"
+  n.times {|r| rolls.insert(r, dice.send(rolldice_methods[d]))}
 
-n.times {|r| rolls.insert(r, dice.send(rolldice_methods[d]))}
+  rolls.each {|r| freq[r] += 1}
 
-rolls.each {|r| freq[r] += 1}
+  puts "You've rolled dice for #{n} times. And the result is: \n"
 
-freq.sort.each {|k, v| puts "#{k}: \t #{v}"}
+  puts   '════════╦════════════════════════════════'
+  puts   '  Tile  ║ Frequency '
+
+  freq.sort.each do |k, v|
+    puts '════════╬════════════════════════════════'
+    puts "   #{k}\t║  #{v} "
+  end
+  puts   '════════╩════════════════════════════════'
+else
+  print 'Wrong input!'
+end
